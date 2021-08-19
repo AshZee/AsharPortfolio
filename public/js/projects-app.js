73be36154e75132b651205576c5429a2e9b01a88
Ashar <ashzee10000@gmail.com>
2021-08-19 17:30:26 -0500

    Committed

--- public/js/projects-app.js
+++ public/js/projects-app.js
@@ -1,14 +1,55 @@
 const scrollable = document.querySelector(".scrollable");
 const personalProjects = document.querySelector(".personalProjects");
 const header = document.querySelector("header");
+const websiteLogoP = document.querySelector(".websiteLogo p");
+const Ashar = document.querySelector(".Ashar");
+const Alvany = document.querySelector(".Alvany");
+const fountainPen = document.querySelector(".websiteLogo svg");
 
+console.log(header.offsetWidth);
+
+const logoP = "ASHARALVANY";
+const space = "&nbsp;";
+let buff = "&lt;";
+let buff1 = "/&gt;";
+let buff2 = "";
+let buff3 = 0;
+let buff4 = "";
 let lastPos = 0;
 let observerOverride;
+let logoChanged = false;
 let scrollFlag = false;
 const options = {
     root: null,
     threshold: 0.25
 }
+function logoAnimation(){
+    for(let i = 0; i < logoP.length; i++){
+        if (i < 5){
+            setTimeout(() =>{
+                buff2 = buff2 + logoP[i];
+                Ashar.innerHTML = buff2;
+                console.log(buff2)
+            }, 500 * i)
+        }
+        else if (i >= 5){
+            setTimeout(() =>{
+                buff4 = buff4 + logoP[i];
+                Alvany.innerHTML = buff4;
+                console.log(buff4)
+            }, 500 * i)
+            buff3 = i;
+        }
+    }
+
+    setTimeout(() =>{
+        rect = Ashar.getBoundingClientRect();
+        rect1 = Alvany.getBoundingClientRect();
+        fountainPen.style.left = ((Math.abs(rect.right - rect1.left)/2) + rect.left + Ashar.offsetWidth)+ "px";
+        console.log((Math.abs(rect.right - rect1.left) + rect.left + Ashar.offsetWidth)+ "px");
+        fountainPen.style.opacity = "1";
+    }, 500 * buff3);
+}   
 
 const observer = new IntersectionObserver((entries, observer) =>{
     entries.forEach(entry => {
@@ -17,6 +58,10 @@ const observer = new IntersectionObserver((entries, observer) =>{
         if(entry.isIntersecting){
             observerOverride = false;
             personalProjects.style.opacity = "0";
+            if (!logoChanged){
+            logoAnimation();
+            logoChanged = true;
+            }
         }
         else{
             observerOverride = true;
